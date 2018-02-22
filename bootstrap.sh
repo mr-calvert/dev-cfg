a17f08feed10c09e4e447e3eb97b0bcbdf08e4a5
Scott Calvert <scalvert@onepost.net>
2018-02-22 13:37:23 -0800

    Make bootstrap download timestamped repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,12 @@
-echo "Hello from bootstrap"
+#!/usr/bin/env bash
+
+dt=`date +"%FT%H%M%S"`
+mkdir -p ~/.dev-cfg/$dt
+cd ~/.dev-cfg/$dt
+
+curl https://codeload.github.com/mr-calvert/dev-cfg/zip/master -O
+unzip master
+rm master
+
+mkdir repo
+mv dev-cfg-master/* repo/
