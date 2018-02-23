c30450068ec6c6bfa1a4d5c61467ed5905d6f4be
Scott Calvert <scalvert@onepost.net>
2018-02-23 09:43:01 -0800

    First pass at configuring ssh

---
+++ init/ssh.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "Ensuring ~/.ssh exists"
+mkdir -p ~/.ssh
+
+echo "Copying config into ~/.ssh"
+cp current/repo/dots/ssh/config ~/.ssh
+
+if [ ! -f ~/.ssh/id_rsa ]; then
+  echo "Generating ssh identity id_rsa"
+  ssh-keygen -t rsa -b 4096 -C "scalvert@splunk.com" -N ""
+  
+  echo "Public key for pasting"
+  echo "=========="
+  cat ~/.ssh/id_rsa.pub
+  echo "=========="
+fi
