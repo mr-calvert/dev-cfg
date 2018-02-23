9888fc941f243f79f94f94c80bd3bacd59550cb2
Scott Calvert <scalvert@onepost.net>
2018-02-23 09:11:36 -0800

    Fix path error addressing env specific inits

--- init/init.sh
+++ init/init.sh
@@ -1,4 +1,4 @@
 # wraps execution of an environment specific setup script to archive a runlog
 
 echo "==> $1 init run `date '+%c'`" >> ~/.dev-cfg/current/runlogs/$1.log
-source ~/.dev-cfg/current/init/$1 2>&1 |tee -a ~/.dev-cfg/current/runlogs/$1.log
+source ~/.dev-cfg/current/repo/init/$1.sh 2>&1 |tee -a ~/.dev-cfg/current/runlogs/$1.log
