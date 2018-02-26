e8ec3280a2318344de0bbc6243f8cb7f770b632d
Scott Calvert <scalvert@onepost.net>
2018-02-26 10:23:28 -0800

    Configure git identity
    Default git identity is Scott Calvert<scalvert@splunk.com>
    
    Created a command alias which sets a project level configuration tying
    that project to Scott Calvert<scalvert@onepost.net>
    
    So to configure a project as personal go to the work directory and:
    git personalprofile

---
+++ init/git.sh
@@ -0,0 +1,4 @@
+#!/user/bin/env bash
+
+echo "Copying .gitconfig into ~"
+cp ~/.dev-cfg/current/repo/dots/git/.gitconfig ~
--- init/unix.sh
+++ init/unix.sh
@@ -2,3 +2,6 @@
 
 echo "Configuring ssh"
 source ~/.dev-cfg/current/repo/init/ssh.sh
+
+echo "Configuring git"
+source ~/.dev-cfg/current/repo/init/git.sh
