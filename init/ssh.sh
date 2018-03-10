bd4648c2af199069a10dc85c1ce0ce8a3a260a67
Scott Calvert <scalvert@onepost.net>
2018-03-10 00:00:05 -0800

    Fixup ssh key permissions

--- init/ssh.sh
+++ init/ssh.sh
@@ -9,6 +9,10 @@ cp current/repo/dots/ssh/config ~/.ssh
 if [ ! -f ~/.ssh/id_rsa ]; then
   echo "Generating ssh identity id_rsa"
   ssh-keygen -t rsa -b 4096 -C "scalvert@splunk.com" -N "" -f ~/.ssh/id_rsa
+
+  chmod 700 ~/.ssh
+  chmod 644 ~/.ssh/id_rsa.pub
+  chmod 600 ~/.ssh/id_rsa
   
   echo "Public key for pasting"
   echo "=========="
